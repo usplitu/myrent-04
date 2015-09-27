20c94b19db980b44b43937755d8599683a6671ad
John Fitzgerald <usplitu@hotmail.com>
2015-09-27 19:00:47 +0100

    MyRent-02: lab step 06

--- app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceListActivity.java
@@ -3,14 +3,26 @@ package org.wit.myrent.activities;
 import org.wit.myrent.R;
 import org.wit.myrent.app.MyRentApp;
 import org.wit.myrent.models.Portfolio;
+import org.wit.myrent.models.Residence;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ResidenceListActivity extends Activity
 {
   private ListView listView;
   private Portfolio portfolio;
+  private ResidenceAdapter adapter;
 
   @Override
   public void onCreate(Bundle savedInstanceState)
@@ -23,5 +35,42 @@ public class ResidenceListActivity extends Activity
 
     MyRentApp app = (MyRentApp) getApplication();
     portfolio = app.portfolio;
+
+    adapter = new ResidenceAdapter(this, portfolio.residences);
+    listView.setAdapter(adapter);
+  }
+
+}
+
+class ResidenceAdapter extends ArrayAdapter<Residence>
+{
+  private Context context;
+
+  public ResidenceAdapter(Context context, ArrayList<Residence> residences)
+  {
+    super(context, 0, residences);
+    this.context = context;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent)
+  {
+    LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    if (convertView == null)
+    {
+      convertView = inflater.inflate(R.layout.list_item_residence, null);
+    }
+    Residence res = getItem(position);
+
+    TextView geolocation = (TextView) convertView.findViewById(R.id.residence_list_item_geolocation);
+    geolocation.setText(res.geolocation);
+
+    TextView dateTextView = (TextView) convertView.findViewById(R.id.residence_list_item_dateTextView);
+    dateTextView.setText(res.getDateString());
+
+    CheckBox rentedCheckBox = (CheckBox) convertView.findViewById(R.id.residence_list_item_isrented);
+    rentedCheckBox.setChecked(res.rented);
+
+    return convertView;
   }
 }

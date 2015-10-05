46cb9d4d1bff930f02e679f1aac13498bbf66107
John Fitzgerald <usplitu@hotmail>
2015-10-05 10:05:39 +0100

    MyRent-04: lab step 06: IntentHelper.navigateUp

--- app/src/main/java/org/wit/android/helpers/IntentHelper.java
+++ app/src/main/java/org/wit/android/helpers/IntentHelper.java
@@ -3,6 +3,7 @@ import java.io.Serializable;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 
 public class IntentHelper
 {
@@ -24,5 +25,11 @@ public class IntentHelper
     Intent intent = new Intent(parent, classname);
     intent.putExtra(extraID, extraData);
     parent.startActivityForResult(intent, idForResult);
+  }
+
+  public static void navigateUp(Activity parent)
+  {
+    Intent upIntent = NavUtils.getParentActivityIntent(parent);
+    NavUtils.navigateUpTo(parent, upIntent);
   }
 }

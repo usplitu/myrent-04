950aaeb4ca6c14f33a2d48ad2eb817d10cd13fb3
John Fitzgerald <usplitu@hotmail>
2015-10-05 09:52:37 +0100

    MyRent-04: lab step 01

--- app/src/main/java/org/wit/myrent/models/Residence.java
+++ app/src/main/java/org/wit/myrent/models/Residence.java
@@ -4,6 +4,9 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Residence
 {
   public UUID id;
@@ -12,16 +15,34 @@ public class Residence
   public Date    date;
   public boolean rented;
 
+  private static final String JSON_ID             = "id"            ;
+  private static final String JSON_GEOLOCATION    = "geolocation"   ;
+  private static final String JSON_DATE           = "date"          ;
+  private static final String JSON_RENTED         = "rented"        ;
+
   public Residence()
   {
-    id = UUID.randomUUID();
-    date = new Date();
-    geolocation = "52.253456,-7.187162";
+    this.id          = UUID.randomUUID();
+    this.date        = new Date();
+    this.geolocation = "";
   }
 
-  public void setGeolocation(String geolocation)
+  public Residence(JSONObject json) throws JSONException
   {
-    this.geolocation = geolocation;
+    id            = UUID.fromString(json.getString(JSON_ID));
+    geolocation   = json.getString(JSON_GEOLOCATION);
+    date          = new Date(json.getLong(JSON_DATE));
+    rented        = json.getBoolean(JSON_RENTED);
+  }
+
+  public JSONObject toJSON() throws JSONException
+  {
+    JSONObject json = new JSONObject();
+    json.put(JSON_ID            , id.toString());
+    json.put(JSON_GEOLOCATION   , geolocation);
+    json.put(JSON_DATE          , date.getTime());
+    json.put(JSON_RENTED        , rented);
+    return json;
   }
 
   public String getDateString()
@@ -29,3 +50,8 @@ public class Residence
     return "Registered: " + DateFormat.getDateTimeInstance().format(date);
   }
 }
+
+  public void setGeolocation(String geolocation)
+  {
+    this.geolocation = geolocation;
+  }

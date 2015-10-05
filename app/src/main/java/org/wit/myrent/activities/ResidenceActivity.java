eaa1dc6f14c2bf831516f415b4c6a5b8576abbb9
John Fitzgerald <usplitu@hotmail>
2015-10-05 10:18:17 +0100

    MyRent-04: lab step 07; impl navigateUp in activity

--- app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceActivity.java
@@ -1,15 +1,17 @@
 package org.wit.myrent.activities;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -18,54 +20,52 @@ import org.wit.myrent.app.MyRentApp;
 import org.wit.myrent.models.Portfolio;
 import org.wit.myrent.models.Residence;
 
-import java.util.UUID;
-
-
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.UUID;
+import static org.wit.android.helpers.IntentHelper.navigateUp;
 
-import android.app.DatePickerDialog;
-import android.view.View;
-
-public class ResidenceActivity extends Activity implements
-    TextWatcher,
-    CompoundButton.OnCheckedChangeListener,
-    View.OnClickListener,
-    DatePickerDialog.OnDateSetListener
+public class ResidenceActivity extends Activity implements TextWatcher, OnCheckedChangeListener, View.OnClickListener, DatePickerDialog.OnDateSetListener
 {
   private EditText geolocation;
-  private Residence residence;
-
   private CheckBox rented;
-  private Button dateButton;
+  private Button   dateButton;
+
+  private Residence residence;
 
   private Portfolio portfolio;
 
   @Override
-  protected void onCreate(Bundle savedInstanceState)
+  public void onCreate(Bundle savedInstanceState)
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_residence);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
 
     geolocation = (EditText) findViewById(R.id.geolocation);
+    dateButton  = (Button)   findViewById(R.id.registration_date);
+    rented      = (CheckBox) findViewById(R.id.isrented);
+
     residence = new Residence();
 
-    // Register a TextWatcher in the EditText geolocation object
     geolocation.addTextChangedListener(this);
+    geolocation.setText(residence.geolocation);
 
-    dateButton = (Button) findViewById(R.id.registration_date);
-    rented = (CheckBox) findViewById(R.id.isrented);
+    rented     .setChecked(residence.rented);
+    rented     .setOnCheckedChangeListener(this);
 
     MyRentApp app = (MyRentApp) getApplication();
     portfolio = app.portfolio;
-
     UUID resId = (UUID) getIntent().getExtras().getSerializable("RESIDENCE_ID");
     residence = portfolio.getResidence(resId);
     if (residence != null)
     {
       updateControls(residence);
     }
+
+    dateButton  .setOnClickListener(this);
+
   }
 
   public void updateControls(Residence residence)
@@ -73,57 +73,30 @@ public class ResidenceActivity extends Activity implements
     geolocation.setText(residence.geolocation);
     rented.setChecked(residence.rented);
     dateButton.setText(residence.getDateString());
-    dateButton.setOnClickListener(this);
   }
 
   @Override
-  public boolean onCreateOptionsMenu(Menu menu)
-  {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.myrent, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item)
-  {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings)
-    {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
-  @Override
-  public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2)
-  {
-
-  }
-
-  @Override
-  public void onTextChanged(CharSequence charSequence, int i, int i1, int i2)
-  {
-
-  }
-
-  @Override
-  public void afterTextChanged(Editable editable)
-  {
-    residence.setGeolocation(editable.toString());
-  }
-
-  @Override
-  public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked)
+  public void onCheckedChanged(CompoundButton arg0, boolean isChecked)
   {
     Log.i(this.getClass().getSimpleName(), "rented Checked");
     residence.rented = isChecked;
+  }
+
+  @Override
+  public void afterTextChanged(Editable c)
+  {
+    Log.i(this.getClass().getSimpleName(), "geolocation " + c.toString());
+    residence.setGeolocation(c.toString());
+  }
+
+  @Override
+  public void beforeTextChanged(CharSequence arg0, int arg1, int arg2, int arg3)
+  {
+  }
+
+  @Override
+  public void onTextChanged(CharSequence arg0, int arg1, int arg2, int arg3)
+  {
   }
 
   @Override
@@ -131,9 +104,8 @@ public class ResidenceActivity extends Activity implements
   {
     switch (v.getId())
     {
-      case R.id.registration_date:
-        Calendar c = Calendar.getInstance();
-        DatePickerDialog dpd = new DatePickerDialog(this, this, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+      case R.id.registration_date      : Calendar c = Calendar.getInstance();
+        DatePickerDialog dpd = new DatePickerDialog (this, this, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
         dpd.show();
         break;
     }
@@ -152,4 +124,16 @@ public class ResidenceActivity extends Activity implements
     super.onPause();
     portfolio.saveResidences();
   }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    switch (item.getItemId())
+    {
+      case android.R.id.home:  navigateUp(this);
+        return true;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
 }

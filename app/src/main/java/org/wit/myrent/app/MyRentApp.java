18ae83e285b26a51e5a59065c0f128c753651c44
John Fitzgerald <usplitu@hotmail>
2015-10-05 09:59:55 +0100

    MyRent-04: lab step 04; MyRentApp

--- app/src/main/java/org/wit/myrent/app/MyRentApp.java
+++ app/src/main/java/org/wit/myrent/app/MyRentApp.java
@@ -1,19 +1,25 @@
 package org.wit.myrent.app;
 
-import org.wit.myrent.models.Portfolio;
-import android.app.Application;
 import static org.wit.android.helpers.LogHelpers.info;
+
+import org.wit.myrent.models.Portfolio;
+import org.wit.myrent.models.PortfolioSerializer;
+
+import android.app.Application;
 
 public class MyRentApp extends Application
 {
+  private static final String FILENAME = "portfolio.json";
+
   public Portfolio portfolio;
 
   @Override
   public void onCreate()
   {
     super.onCreate();
-    portfolio = new Portfolio();
+    PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+    portfolio = new Portfolio(serializer);
 
-    info(this, "MyRent app launched");
+    info(this, "RentControl app launched");
   }
 }
